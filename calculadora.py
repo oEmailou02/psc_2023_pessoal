c730f0e85fa85e457284539d385c05aa3560eea8
Emilio Gabriel <emiliogabrielsantos123@gmail.com>
2023-09-04 18:54:59 -0300

    porcentagem implementada

--- calculadora.py
+++ calculadora.py
@@ -6,3 +6,8 @@ def multiplicar (a, b):
     return a * b
 def dividir (a, b):
     return a / b
+def porcentagem ():
+    numero = int(input("Digite o valor: "))
+    pct = int(input("Digite a porcentagem: "))
+    numero = numero / 100
+    return numero*pct
